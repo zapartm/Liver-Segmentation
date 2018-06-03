db8ef14ffe1b96c4bb1651d08675e8f7b917606b
zapartm <zapartm@student.mini.pw.edu.pl>
2018-06-04 01:22:59 +0200

    major refactor

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import dicom
 import os
 import numpy
-import cv2
+import copy
 import numpy as np
-from matplotlib import pyplot, cm
+import matplotlib.pyplot as plt
 
-PathDicom = "./prez/"
+# PARAMS
+# eps - "czułość" algorytmu, większa wartośc - większy obszar zostanie obięty
+# PathDicom - ścieżka do katalogu z dicomami
+# NUM = indeks interesującego nas przekroju
+eps = 25
+PathDicom = "./dicoms/"
+NUM = 50
+
 lstFilesDCM = []  # create an empty list
 for dirName, subdirList, fileList in os.walk(PathDicom):
     for filename in fileList:
@@ -28,20 +35,10 @@ z = numpy.arange(0.0, (ConstPixelDims[2]+1)*ConstPixelSpacing[2], ConstPixelSpac
 # The array is sized based on 'ConstPixelDims'
 ArrayDicom = numpy.zeros(ConstPixelDims, dtype=RefDs.pixel_array.dtype)
 
-# loop through all the DICOM files
+# read the file and store image data
 for filenameDCM in lstFilesDCM:
-    # read the file
     ds = dicom.read_file(filenameDCM)
-    # store the raw image data
     ArrayDicom[:, :, lstFilesDCM.index(filenameDCM)] = ds.pixel_array
-
-cols = len(ArrayDicom);
-#
-# pyplot.figure(dpi=300)
-# pyplot.axes().set_aspect('equal', 'datalim')
-# pyplot.set_cmap(pyplot.gray())
-# pyplot.pcolormesh(x, y, numpy.flipud(ArrayDicom[:, :, 2]))
-# pyplot.show()
 
 
 def get8n(x, y, shape):
@@ -91,69 +88,83 @@ def get8n(x, y, shape):
 
     return out
 
-# def region_growing(img, start_point, seed):
-#     list = []
-#     outimg = np.zeros_like(img)
-#     list.append((start_point[0], start_point[1]))
-#     processed = []
-#     factor = 4
-#     counter = 0
-#     while(len(list) > 0):
-#         pix = list[0]
-#         outimg[pix[0], pix[1]] = 255
-#         for coord in get8n(pix[0], pix[1], img.shape):
-#             if outimg[coord[0], coord[1]] != 255 and img[coord[0], coord[1]] > seed - factor and img[coord[0], coord[1]] < seed + factor:
-#                 outimg[coord[0], coord[1]] = 255
-#                 list.append(coord)
-#         list.pop(0)
-#         if counter == 100:
-#             counter = 0
-#             cv2.imshow("progress",outimg)
-#             cv2.waitKey(1)
-#         counter += 1
-#     return outimg
-
 def region_growing(img, start_point, seed):
+    print("processing...")
     list = []
     outimg = np.zeros_like(img)
+    max = -2000
     for row in range(len(outimg)):
         for col in range(len(outimg[0])):
             outimg[row, col] = img[row, col]
+            if img[row,col] > max:
+                max = img[row,col]
 
     list.append((start_point[0], start_point[1]))
     processed = []
-    factor = 2
-    counter = 0
+    factor = eps
     while(len(list) > 0):
         pix = list[0]
-        outimg[pix[0], pix[1]] = 255
+        outimg[pix[0], pix[1]] = max
         for coord in get8n(pix[0], pix[1], img.shape):
-            if outimg[coord[0], coord[1]] != 255 and img[coord[0], coord[1]] > seed - factor and img[coord[0], coord[1]] < seed + factor:
-                outimg[coord[0], coord[1]] = 255
+            if outimg[coord[0], coord[1]] != max and img[coord[0], coord[1]] > seed - factor and img[coord[0], coord[1]] < seed + factor:
+                outimg[coord[0], coord[1]] = max
                 list.append(coord)
         list.pop(0)
-        if counter == 100:
+
+    print("Region growing: done. ")
+    print("Starting post processing...")
+    outimg2 = copy.deepcopy(outimg)
+    for row in range(len(outimg)):
+        for col in range(len(outimg[0])):
             counter = 0
-            cv2.imshow("progress",outimg)
-            cv2.waitKey(1)
-        counter += 1
-    return outimg
+            for coord in get8n(row, col, img.shape):
+                if(outimg[coord[0], coord[1]] == max):
+                    counter = counter + 1
+            if counter > 2:
+                outimg2[row, col] = max
+
+    print("all: done")
+    return outimg2
 
 
-def on_mouse(event, x, y, flags, params):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        cv2.destroyWindow('Input')
+def get_pixels_hu(scans, referenceDM):
+    # image = np.stack([s.pixel_array for s in scans])
+    image = scans.pixel_array
+    # Convert to int16 (from sometimes int16),
+    # should be possible as values should always be low enough (<32k)
+    image = image.astype(np.int16)
+
+    # Set outside-of-scan pixels to 1
+    # The intercept is usually -1024, so air is approximately 0
+    image[image == -2000] = 0
+
+    # Convert to Hounsfield units (HU)
+    intercept = referenceDM.RescaleIntercept
+    slope = referenceDM.RescaleSlope
+
+    if slope != 1:
+        image = slope * image.astype(np.float64)
+        image = image.astype(np.int16)
+
+    image += np.int16(intercept)
+    return np.array(image, dtype=np.int16)
+
+ds2 = dicom.read_file(lstFilesDCM[NUM])
+image = get_pixels_hu(ds2, RefDs)
+
+ax = plt.gca()
+fig = plt.gcf()
+implot = ax.imshow(image, cmap='gray')
+plt.axis('off')
+def onclick(event):
+    if event.xdata != None and event.ydata != None:
+        x = int(event.xdata)
+        y = int(event.ydata)
         seed = image[y,x]
         print('Seed: ' + str(x) + ', ' + str(y), seed)
         img2 = region_growing(image, (y,x), seed)
-        cv2.destroyWindow("progress")
-        cv2.imshow('proccessing image', img2)
-        cv2.waitKey()
+        implot = ax.imshow(img2, cmap='gray')
+        plt.show()
 
-image = cv2.imread('3.png', 0)
-# image = ArrayDicom[:, :, 50]
-
-cv2.namedWindow('Input')
-cv2.setMouseCallback('Input', on_mouse, 0, )
-cv2.imshow('Input', image)
-cv2.waitKey()
+cid = fig.canvas.mpl_connect('button_press_event', onclick)
+plt.show()
